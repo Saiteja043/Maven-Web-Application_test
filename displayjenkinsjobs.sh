2f34edc5386d402cfdc8ebd54b0b5ae5ed500dc9
Saiteja <rayalasaiteja1@gmail.com>
2026-05-23 16:24:53 +0530

    updated sh file

--- displayjenkinsjobs.sh
+++ displayjenkinsjobs.sh
@@ -14,7 +14,7 @@ read jobName
 
 java -jar "/h/DevOps_Softwares/Installed_files/Jenkins_cli/jenkins-cli.jar" \
 -s http://192.168.29.199:8080/ \
--auth Jenkins:112e8f239773ea69dc480703b0ff1ba4ec build $jobName
+-auth Jenkins:112e8f239773ea69dc480703b0ff1ba4ec build $jobName -p BranchName=master
 
 echo "Building the $jobName Job" 
 
